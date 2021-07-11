7fc36773ee57e4a1bec0933f2e8cef150b269b54
Donald Wu <yeukfei02@gmail.com>
2021-07-11 21:21:42 +0800

    fix: 🐛 use devDependencies in package.json

--- src/schema/typeDefs.ts
+++ src/schema/typeDefs.ts
@@ -1,5 +1,60 @@
-import { importSchema } from 'graphql-import';
+import { gql } from 'apollo-server';
 
-const typeDefs = importSchema('src/schema/schema.graphql');
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+    user(id: ID!): User!
+  }
+
+  type Mutation {
+    signup(data: Signup!): SignupResult!
+    login(data: Login!): LoginResult!
+    updateUser(id: ID!, data: UpdateUser!): UpdateUserResult!
+    deleteUser(id: ID!): DeleteUserResult!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    password: String!
+    name: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type SignupResult {
+    message: String!
+  }
+
+  type LoginResult {
+    message: String!
+    token: String!
+  }
+
+  type UpdateUserResult {
+    message: String!
+  }
+
+  type DeleteUserResult {
+    message: String!
+  }
+
+  input Signup {
+    email: String!
+    password: String!
+    name: String!
+  }
+
+  input Login {
+    email: String!
+    password: String!
+  }
+
+  input UpdateUser {
+    email: String!
+    password: String!
+    name: String!
+  }
+`;
 
 export default typeDefs;
